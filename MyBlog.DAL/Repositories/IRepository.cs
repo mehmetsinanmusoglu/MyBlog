88859e605b4533a809db3c4d89c3ab04b5ca8d62
mehmet.musoglu <mehmetsinanmusoglu@gmail.com>
2019-09-12 11:47:12 +0300

    Dal Tamamlandı

--- MyBlog.DAL/Repositories/IRepository.cs
+++ MyBlog.DAL/Repositories/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace EasyBlog.DAL.Repositories
+namespace MyBlog.DAL.Repositories
 {
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
---
+++ MyBlog.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.DAL.Repositories;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.UnitOfWork
+{
+    public interface IUnitOfWork<TContext> : IDisposable where TContext :DbContext
+    {
+        TContext DbContext { get; }
+        IRepository<TEntity> GetRespository<TEntity>() where TEntity : BaseEntity;
+        int SaveChanges();
+    }
+}
